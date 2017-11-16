7a92df3e13b62c8a07ac05c7ece5341dd0607608
Gray Wheeler <gwheeler21@brophybroncos.org>
2017-11-16 01:24:15 +0000

    Add styling to personal website

---
+++ that_was_easy/main.js
@@ -0,0 +1,9 @@
+- var thatWasEasy;
+  - var thatWasEasy = new Audio("that_was_easy.mp3");
+- thatWasEasy.play();
+
++ function sayThatWasEasy() {
++   var thatWasEasy = new Audio("that_was_easy.mp3");
++   thatWasEasy.play();
++ }
+$(
